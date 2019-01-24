c4f6204fc3925f09c47afc37111e2c047e84c5f0
Gwynhegg <mister.chepurov24@gmail.com>
2019-01-24 11:25:59 +0500

    MazeProject

---
+++ Maze/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Maze
+{
+    partial class Maze
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.textHeight = new System.Windows.Forms.TextBox();
+            this.textWidth = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(713, 32);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreate.TabIndex = 0;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // textHeight
+            // 
+            this.textHeight.Location = new System.Drawing.Point(688, 61);
+            this.textHeight.Name = "textHeight";
+            this.textHeight.Size = new System.Drawing.Size(100, 20);
+            this.textHeight.TabIndex = 1;
+            this.textHeight.Text = "Height";
+            // 
+            // textWidth
+            // 
+            this.textWidth.Location = new System.Drawing.Point(688, 87);
+            this.textWidth.Name = "textWidth";
+            this.textWidth.Size = new System.Drawing.Size(100, 20);
+            this.textWidth.TabIndex = 2;
+            this.textWidth.Text = "Width";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(526, 425);
+            this.panel1.TabIndex = 3;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // Maze
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textWidth);
+            this.Controls.Add(this.textHeight);
+            this.Controls.Add(this.buttonCreate);
+            this.Name = "Maze";
+            this.Text = "Maze";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.TextBox textHeight;
+        private System.Windows.Forms.TextBox textWidth;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Maze/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maze
+{
+    public partial class Maze : Form
+    {
+        bool isPath = false;
+        int width, height, randomFactor=3, drawX;
+        Random fact = new Random();
+        Graphics g;
+        Pen pen1 = new Pen(Color.Black, 2);
+        Pen pen2 = new Pen(Color.Red, 1);
+        int X, Y;
+        public Maze()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            g = panel1.CreateGraphics();
+            g.Clear(Color.White);       
+            height = Int32.Parse(textHeight.Text);
+            width = Int32.Parse(textWidth.Text);
+            drawX = (panel1.Height-50) / height;
+            int exit = fact.Next(width*3/8,width*5/8);
+            g.DrawLine(pen1, 10, 10, 10+exit * drawX, 10);
+            g.DrawLine(pen1, 10 + exit * drawX+drawX, 10, 10 + width * drawX, 10);
+            g.DrawLine(pen1, 10, 10, 10, 10+height*drawX);
+            g.DrawLine(pen1, 10+width*drawX, 10, 10+width*drawX, 10+height*drawX);
+            int[,] mazeQuantity = new int[height, width];
+            byte[,] mazeWalls = new byte[height, width];
+            // 0 - проход, 1 -стена справа, 2 - стена снизу, 3 - стены снизу и справа
+            createFirstRow(mazeQuantity, mazeWalls);
+            createLowCorner(mazeQuantity, mazeWalls, 0);
+            for (int i = 1; i < height - 1; i++)
+            {
+                createAnotherRow(mazeQuantity, mazeWalls, i);
+                createLowCorner(mazeQuantity, mazeWalls, i);
+            }
+            createAnotherRow(mazeQuantity, mazeWalls, height - 1);
+            createLastRow(mazeQuantity, mazeWalls);
+            printMaze(mazeWalls);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPath)
+            {
+                g.DrawLine(pen2,X, Y, e.X, e.Y);
+                X = e.X;
+                Y = e.Y;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!isPath && e.Y>=height*drawX-drawX/2)
+            {
+                X = e.X;
+                Y = e.Y;
+                isPath = true;
+            } else
+            {
+                isPath = false;
+            }
+        }
+
+        private void createLastRow(int[,] quant, byte[,] walls)
+        {
+
+            int numQuant = quant[height-1, 0];
+            for (int i = height-1; i >0; i--)
+            {
+                if (quant[height - 1, i] != quant[height - 1, i - 1]) walls[height - 1, i - 1] = 0;
+            }
+        }
+        private void createAnotherRow(int[,] quant, byte[,] walls,int numofRow)
+        {
+            int numQuant = numofRow*width;
+            for (int i = 0; i < width; i++)
+            {
+                if (walls[numofRow - 1, i] != 2 && walls[numofRow - 1, i] != 3)
+                {
+                    quant[numofRow, i] = quant[numofRow - 1, i];
+                } else
+                {
+                    quant[numofRow, i] = numQuant;
+                    numQuant++;
+                }
+            }
+            numQuant = quant[numofRow, 0];
+            for (int i = 1; i < width; i++)
+            {
+                if (quant[numofRow, i] != quant[numofRow, i - 1])
+                {
+                    if (fact.Next(randomFactor) == 1)
+                    {
+                        walls[numofRow, i-1] = 1;
+                        numQuant=quant[numofRow,i];
+                    }
+                    else
+                    {
+                        quant[numofRow, i] = numQuant;
+                    }
+                } else
+                {
+                    walls[numofRow, i - 1] = 1;
+                }
+            }
+        }
+        private void createLowCorner(int[,] quant, byte[,] walls, int numOfRow)
+        {
+            int leftCorner = 0,rightCorner;
+            int checkQuant = quant[numOfRow, leftCorner];
+            int noWall;
+            for (int i = 0; i < width; i++)
+            {
+                if (quant[numOfRow, i] != checkQuant || i == width - 1)
+                {
+                        noWall = fact.Next(leftCorner, i);
+                        if (i == width - 1 && quant[numOfRow, i] == checkQuant) rightCorner = i + 1; else rightCorner = i;
+                        for (int j = leftCorner; j < rightCorner; j++)
+                        {
+                            if (j != noWall)
+                            {
+                                if (walls[numOfRow, j] == 0) walls[numOfRow, j] = 2; else walls[numOfRow, j] = 3;
+                            } 
+                        }                    
+                    checkQuant = quant[numOfRow, i];
+                    leftCorner = i;
+                
+                }
+                
+            }
+            
+        }    
+        private void createFirstRow(int[,] quant, byte[,] walls)
+        {
+            int i = 1;
+            for (int j = 0; j < width; j++)
+            {
+                if (fact.Next(randomFactor)==1)
+                {
+                    quant[0, j] = i;
+                    walls[0, j] = 1;
+                    i++;
+                } else
+                {
+                    quant[0, j] = i;
+                }
+            }
+        }
+        private void printMaze(byte[,] a)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (a[i, j] == 1)
+                    {
+                        g.DrawLine(pen1, 10 + (j + 1) * drawX, 10 + i * drawX, 10 + (j + 1) * drawX, 10 + (i + 1) * drawX);
+                    } else if (a[i, j] == 2)
+                    {
+                        g.DrawLine(pen1, 10 + j * drawX, 10 + (i + 1) * drawX, 10 + (j + 1) * drawX, 10 + (i + 1) * drawX);
+                    }
+                    else if (a[i, j] == 3)
+                    {
+                        g.DrawLine(pen1, 10 + (j + 1) * drawX, 10 + i * drawX, 10 + (j + 1) * drawX, 10 + (i + 1) * drawX);
+                        g.DrawLine(pen1, 10 + j * drawX, 10 + (i + 1) * drawX, 10 + (j + 1) * drawX, 10 + (i + 1) * drawX);
+                    }
+                    Console.Write(a[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
